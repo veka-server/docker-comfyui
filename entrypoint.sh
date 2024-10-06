4d60a03ca31fc2d8162bf62a82282f787df77d57
Veka <veka610@gmail.com>
2024-10-07 00:01:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Ajouter la règle iptables pour bloquer les connexions sortantes
+iptables -I OUTPUT -o eth0 -j DROP
+
 pushd /app/comfyui
 source venv/bin/activate
 python3 main.py "$@"
