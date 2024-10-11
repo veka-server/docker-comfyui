6939d8d2cef03d6849c569c930f6c03450896aa0
Veka <veka610@gmail.com>
2024-10-11 17:16:12 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # 1. Ajouter la règle iptables pour bloquer les connexions sortantes
-iptables -I OUTPUT -o eth0 -j DROP
+# iptables -I OUTPUT -o eth0 -j DROP
 
-# 3. Lancer ComfyUI
+# 2. Lancer ComfyUI
 pushd /app/comfyui
 source venv/bin/activate
 python3 main.py "$@"
 popd
 
 # 2. Démarrer supervisord pour gérer Xvfb, x11vnc, noVNC, et Fluxbox
-# /usr/bin/supervisord -c /app/supervisor/supervisord.conf &
+/usr/bin/supervisord -c /app/supervisor/supervisord.conf &
 
 # 4. Garder le conteneur en cours d'exécution
-# wait
+ wait
