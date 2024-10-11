9846aeaa1e4dd7645d6fd116563705cdcb5386d2
Veka <veka610@gmail.com>
2024-10-11 17:41:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-# 1. Ajouter la règle iptables pour bloquer les connexions sortantes
-# iptables -I OUTPUT -o eth0 -j DROP
+# 1. Vérifier si la variable d'environnement VNC_PASSWORD est définie
+if [ -z "$VNC_PASSWORD" ]; then
+    echo "Aucun mot de passe VNC défini. Utilisation d'un mot de passe par défaut."
+    VNC_PASSWORD="default_password"  # Mot de passe par défaut
+fi
 
-# 2. Démarrer supervisord pour gérer Xvfb, x11vnc, noVNC, et Fluxbox
+# 2. Configurer le mot de passe pour x11vnc
+echo "$VNC_PASSWORD" | x11vnc -storepasswd - /app/.vnc/passwd
+
+# 3. Démarrer supervisord pour gérer Xvfb, x11vnc, noVNC, et Fluxbox
 /usr/bin/supervisord -c /app/supervisor/supervisord.conf &
 
-# 3. Lancer ComfyUI
+# 4. Lancer ComfyUI
 pushd /app/comfyui
 source venv/bin/activate
 python3 main.py --listen 0.0.0.0 --port 8188 --preview-method auto
 popd
 
-# 4. Garder le conteneur en cours d'exécution
- wait
+# 5. Garder le conteneur en cours d'exécution
+wait
