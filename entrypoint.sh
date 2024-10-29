ced74fd891dd606a78dd8f21897ef77560028358
Veka <veka610@gmail.com>
2024-10-29 18:16:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ chmod 600 /app/.vnc/passwd
 # 4. Lancer ComfyUI
 pushd /app/comfyui
 source venv/bin/activate
-python3 main.py --listen 0.0.0.0 --port 8188 --lowvram
+python3 main.py --listen 0.0.0.0 --port 8188 --lowvram --disable-smart-memory
 popd
 
 # 5. Garder le conteneur en cours d'exécution
