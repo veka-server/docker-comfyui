fc5540660443f68d08d94602374b77677ca87918
Veka <veka610@gmail.com>
2026-08-19 12:51:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Lancer ComfyUI
 pushd /app/comfyui
 # source venv/bin/activate
-python3 main.py --listen 0.0.0.0 --port 8188 --lowvram --disable-smart-memory
+python3 main.py --listen 0.0.0.0 --port 8188 --lowvram --disable-smart-memory --disable-api-nodes
 popd
 
 # 5. Garder le conteneur en cours d'exécution
